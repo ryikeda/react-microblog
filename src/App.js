c8e415502b23eebc3cc4dd10b1769fbc359562bb
rafael ubuntu <ryikeda@outlook.com>
2020-10-18 23:58:25 +0900

    created Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <>Microblog</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Paper, Typography, makeStyles, Link } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(7),
+    margin: theme.spacing(2),
+    backgroundColor: "#f4f4f4",
+  },
+  header: {
+    marginBottom: theme.spacing(3),
+  },
+  Link: {
+    marginRight: theme.spacing(3),
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Paper className={classes.root}>
+        <Typography className={classes.header} variant="h1">
+          Microblog
+        </Typography>
+        <Typography className={classes.header} variant="h5">
+          Get in the Rythmn of blogging!
+        </Typography>
+        <Link className={classes.Link} href="#">
+          Blog
+        </Link>
+        <Link href="#">Add a new post</Link>
+      </Paper>
+    </>
+  );
+};
+
+export default Header;
