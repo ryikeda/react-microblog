0c053368e39b21b30c863d46ab67e1bdf671cc6f
rafael ubuntu <ryikeda@outlook.com>
2020-10-20 15:50:05 +0900

    installed redux-thunk

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
